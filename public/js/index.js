dafa87c700edfa900ab10c9294d20ec03c4486eb
huytocdo <ndhuyspkt@gmail.com>
2019-08-10 16:32:48 +0700

    fix bundle

--- public/js/index.js
+++ public/js/index.js
@@ -67,4 +67,6 @@ if (bookBtn) {
 }
 
 const alertMessage = document.querySelector('body').dataset.alert;
-if (alertMessage) showAlert('success', alertMessage, 20);
+if (alertMessage) {
+  showAlert('success', alertMessage, 20);
+}
